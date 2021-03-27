cbb452f493c1b1f5a3d7187351409cfb4a271230
Jeong-jeong <ghkdvnfld345@naver.com>
2021-03-27 11:45:29 +0900

    수박수박수 효율 답안

--- programmers/1-2.watermelon.js
+++ programmers/1-2.watermelon.js
@@ -1,10 +1,6 @@
 function solution(n) {
-    let answer = '';
-    let handClap = '박수';
-    if (n === 1) answer = '수';
-    else if (n % 2 === 0) { // 짝수일 때
-        answer = '수박'.repeat(n / 2); // '수박'을 입력값 4 / 2 만큼 반복 = 수박 * 2
-    } else answer = '수' + handClap.repeat(Math.floor(n / 2))
-
-    return answer;
+    return '수박'.repeat( (n/2)) + ((n % 2) ? '수' : ''); 
 }
+
+let n = 1;
+console.log(solution(n));
