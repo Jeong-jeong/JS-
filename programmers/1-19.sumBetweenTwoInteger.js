0ec2b969dde3a68c52dbd2f9f849644f35c58a95
Jeong-jeong <ghkdvnfld345@naver.com>
2021-04-14 11:28:03 +0900

    ♻️ sum between two integer best answer I think

--- programmers/1-19.sumBetweenTwoInteger.js
+++ programmers/1-19.sumBetweenTwoInteger.js
@@ -1,18 +1,10 @@
 // 프로그래머스 1단계 두 정수 사이의 합
 
-function solution (a, b) {
-	let sum = 0;
-	if (a < b) {
-			while ( a <= b) {
-					sum += a;
-					a++;
-			}
-	} else if ( a > b) {
-			while ( a >= b) {
-					sum += a;
-					a--;
-			}
-	} else sum = a;
- 
-	return sum;
+function adder(a, b){
+	let result = (a < b) ? a : b; // a < b 이면 return a 아니면 b
+	while (a != b) { // a와 b가 같을 때까지 반복
+		result += (a < b) ? ++a : ++b;
+		// return에 a나 b가 있는 상태에서 작을경우 a를 1씩 증가하며 누적 아니면 b를 누적
+	}    
+	return result;
 }
