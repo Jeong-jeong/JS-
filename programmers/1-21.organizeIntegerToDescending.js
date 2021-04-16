141659e5a949eb4f7fda6fb97c3ef695adc5b51c
Jeong-jeong <ghkdvnfld345@naver.com>
2021-04-16 10:50:22 +0900

    ♻️ efficent answer compares to previous

--- programmers/1-21.organizeIntegerToDescending.js
+++ programmers/1-21.organizeIntegerToDescending.js
@@ -1,4 +1,8 @@
-// 프로그래머스 1단계; 정수 내림차순 배치
 function solution(n) {
-	return +(n.toString().split('').sort( (a, b) => Number(b) - Number(a) ).join(''))
+	return +(n.toString().
+					 split('').
+					 sort().
+					 reverse().
+					 join('')
+					)
 }
