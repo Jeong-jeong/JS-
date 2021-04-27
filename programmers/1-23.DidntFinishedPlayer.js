613cd828b020dd63cd2774c8d8ec722ffbaa7325
Jeong-jeong <ghkdvnfld345@naver.com>
2021-04-27 17:08:07 +0900

    🎨 efficient code to hash map lev 1

--- programmers/1-23.DidntFinishedPlayer.js
+++ programmers/1-23.DidntFinishedPlayer.js
@@ -1,16 +1,8 @@
-function solution(participant, completion) {
-	let pH = new Map();
-	for (let player of participant) {
-			(pH.has(player)) ?
-			pH.set(player, pH.get(player) + 1) :
-			pH.set(player, 1)
-	}
-	
-	for (let player of completion) {
-			pH.set(player, pH.get(player) - 1);
-	}
-	
-	for (let [key, val] of pH) {
-			if (val !== 0) return key
+function solution(p, c) {
+	p.sort()
+	c.sort()
+	while (p.length) {
+			let pp = p.pop()
+			if (pp !== c.pop()) return pp
 	}
 }
