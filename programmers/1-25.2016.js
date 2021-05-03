040f301e17f035d7eceef2382065a89ccd50bddd
Jeong-jeong <ghkdvnfld345@naver.com>
2021-05-03 11:50:08 +0900

    ♻️  Refactor Finding the day in 2016

--- programmers/1-25.2016.js
+++ programmers/1-25.2016.js
@@ -1,17 +1,6 @@
-function solution (a, b) {
-	let date = new Date(2016, `${a - 1}`, `${b}`);
+function solution(a, b) {
+	let week = ['SUN', 'MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT'];
+	let date = new Date(2016, (a - 1), (b));
 	let day = date.getDay();
-	return getDay(day);
-	
-			
-	
-	function getDay(n) {
-			if (n === 0) return 'SUN';
-			else if (n === 1) return 'MON';
-			else if (n === 2) return 'TUE';
-			else if (n === 3) return 'WED';
-			else if (n === 4) return 'THU';
-			else if (n === 5) return 'FRI';
-			else if (n === 6) return 'SAT';
-	}
+	return week[day]
 }
